0504f2706252b214c61e783b9c2c54ea19fdee30
minohi99 <minohi99@gmail.com>
2024-03-12 08:16:39 +0900

    開発環境ブラン

---
+++ src/components/EachPost.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EachPost = () => {
+  return (
+    <article>
+      <a href="post.html">
+        <h3>記事のタイトルあああああああああおおおお</h3>
+      </a>
+    </article>
+  );
+};
+
+export default EachPost;
