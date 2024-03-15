ec9cf2b0a124474a96f18fe7ed18a36023e8013f
minohi99 <minohi99@gmail.com>
2024-03-15 17:48:43 +0900

    PostHeader作成

--- src/app/blog/schedule/page.tsx
+++ src/app/blog/schedule/page.tsx
@@ -1,12 +1,18 @@
 import Container from '@/components/Container';
+import PostHeader from '@/components/PostHeader';
 import { getPostBySlug } from '@/lib/api';
 
 export default async function Schedule() {
-  const { title } = await getPostBySlug('schedule');
+  const { title, publishDate } = await getPostBySlug('schedule');
   return (
     <Container>
-      <h1>{title}</h1>
-      {/* 追加のコンテンツ表示 */}
+      <article>
+        <PostHeader
+          title={title}
+          subtitle="Blog Article"
+          publishDate={publishDate}
+        />
+      </article>
     </Container>
   );
 }
