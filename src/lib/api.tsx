3e407efbeb37edf35b259942a0dfd7fb7cf21dd0
minohi99 <minohi99@gmail.com>
2024-03-21 14:05:17 +0900

    apiファイル内のclientに非nullアサーション演算子を付与

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,8 +1,8 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN,
-  apiKey: process.env.API_KEY,
+  serviceDomain: process.env.SERVICE_DOMAIN!,
+  apiKey: process.env.API_KEY!,
 });
 
 export async function getPostBySlug(slug: string) {
