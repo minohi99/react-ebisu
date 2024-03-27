688d41b3247dbb956a5a11273ec73bafcf6be6e9
minohi99 <minohi99@gmail.com>
2024-03-27 17:47:15 +0900

    JSDocの追加

--- src/libs/api.tsx
+++ src/libs/api.tsx
@@ -1,11 +1,20 @@
 import { SlugObject } from '@/app/types/SlugObject';
 import { createClient } from 'microcms-js-sdk';
 
+/**
+ * microCMSのクライアントを作成
+ */
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN!,
   apiKey: process.env.API_KEY!,
 });
 
+/**
+ * 指定されたスラッグに一致する投稿を取得
+ *
+ * @param {string} slug - 取得したい投稿のスラグ。
+ * @returns {Promise<any>} 指定されたスラグに一致する投稿オブジェクトを返します。
+ */
 export async function getPostBySlug(slug: string) {
   try {
     const post = await client.getList({
@@ -18,6 +27,13 @@ export async function getPostBySlug(slug: string) {
     return post.contents[0];
   } catch (err) {}
 }
+
+/**
+ * 全てのスラッグを取得
+ *
+ * @param {number} [limit=100] - 取得するスラグの最大数。
+ * @returns {Promise<SlugObject[]>} スラグオブジェクトの配列を返します。
+ */
 export async function getAllSlugs(limit = 100): Promise<SlugObject[]> {
   try {
     const slugs = await client.get({
@@ -29,5 +45,21 @@ export async function getAllSlugs(limit = 100): Promise<SlugObject[]> {
     console.log('~~ getAllSlugs ~~');
     console.log(err);
     return [];
+  }
+}
+
+export async function getAllPosts(limit = 100) {
+  try {
+    const posts = await client.get({
+      endpoint: 'blogs',
+      queries: {
+        fields: 'title,slug,eyecatch',
+        orders: 'publishDate',
+        limit: limit,
+      },
+    });
+    return posts.contents;
+  } catch (error) {
+    console.log('🚀 ~ getAllPosts ~ error:', error);
   }
 }
--- src/libs/getImageBuffer.tsx
+++ src/libs/getImageBuffer.tsx
@@ -1,6 +1,16 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
 
+/**
+ * 指定されたソースから画像データのバッファを取得します。
+ *
+ * ソースがHTTP(S) URLの場合、外部サーバーから画像をフェッチし、そのバッファを返します。
+ * ソースがローカルファイルパスの場合、指定されたパスのファイルを読み込み、そのバッファを返します。
+ *
+ * @param {string} src - 画像データのソースURLまたはローカルファイルパス。
+ * @returns {Promise<Buffer>} 画像データのバッファ。
+ */
+
 export async function getImageBuffer(src: string) {
   if (src.startsWith('http')) {
     const res = await fetch(src);
