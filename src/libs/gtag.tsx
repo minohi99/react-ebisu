222737986c9dd6d320d0b97e9caec8ec40e59f98
minohi99 <minohi99@gmail.com>
2024-03-29 18:46:08 +0900

    gtagのグローバルでのカスタム定義

--- src/libs/gtag.tsx
+++ src/libs/gtag.tsx
@@ -1,3 +1,9 @@
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const pageview = (url: string) => {
